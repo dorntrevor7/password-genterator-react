404e64f020a326c37d237060cb0bebbf55ecf032
dorntrevor7 <dorntrevor7@gmail.com>
2022-05-23 21:11:04 -0700

    Password container and components set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Heading from "./Components/Heading.js";
+import Password from "./Components/Password.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Hello World</h2>
+        <Container className="PasswordWrapper">
+          <Col>
+            <Row>
+              <Heading className="Heading" />
+            </Row>
+            <Row>
+              <Password className="Password" />
+            </Row>
+            <Row>
+              <Button variant="outline-success">Generate Password</Button>{" "}
+            </Row>
+          </Col>
+        </Container>
       </header>
     </div>
   );
