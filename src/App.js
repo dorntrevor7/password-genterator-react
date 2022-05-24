e23a0a22c861461e4a1b38e68019b564d26144fa
dorntrevor7 <dorntrevor7@gmail.com>
2022-05-23 22:23:37 -0700

    questions for passwords

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 import Heading from "./Components/Heading.js";
 import Password from "./Components/Password.js";
 import "./App.css";
-
+const Questions = () => {
+  const start = alert("Would you like to generate a new password?");
+  const up = alert("Would you like to upper case characters in your password?");
+  const low = alert("Would you like to lowercase characters in your password?");
+  const num = alert("Would you like numbers in your password?");
+  const special = alert(
+    "Would you like to special characters in your password?"
+  );
+};
 function App() {
   let password = "";
   const generatePassword = () => {
     const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    const specialCase = "!@#$";
+    const specialCase = "!@#$&";
     const numbers = "1234567890";
   };
   return (
